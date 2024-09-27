b2678a494dc06a12ec5b6643a4273a1c0f425ec5
shaun <beingswu@gmail.com>
2024-09-27 17:22:23 +0800

    update llm

--- tasks/srt_translation/llm.py
+++ tasks/srt_translation/llm.py
@@ -79,8 +79,8 @@ def _admin_prompt(target_lan: str, source_lan: Optional[str]) -> str:
         source_lan = "any language and you will detect the language"
     return f"""
 I want you to act as an {target_lan} translator, spelling corrector and improver. 
-Next user will speak to you in {source_lan}, translate it and answer in the corrected and improved version of my text, in Chinese. 
-I want you to replace simplified A0-level words and sentences with more beautiful and elegant, upper level Chinese words and sentences. Keep the meaning same, but make them more literary. 
+Next user will speak to you in {source_lan}, translate it and answer in the corrected and improved version of my text, in {target_lan}. 
+I want you to replace simplified A0-level words and sentences with more beautiful and elegant, upper level {target_lan} words and sentences. Keep the meaning same, but make them more literary. 
 I want you to only reply the correction, the improvements and nothing else, do not write explanations.
 Next user will speak a passage. The passage is divided into multiple lines, each line starting with a number (an Arabic numeral followed by a colon).
 Your translation should also respond in multiple lines, with corresponding numbers at the beginning of each line in the translation.
